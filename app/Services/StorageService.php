2284bab0cd1abfda88381b2bef0d91ebd0d4a2a2
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-06-06 18:55:24 +0800

    feat(storage service): add exists, allExists and download function

--- app/Services/StorageService.php
+++ app/Services/StorageService.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class StorageService
 {
@@ -50,6 +51,34 @@ class StorageService
         throw new StorageContentMissingException();
     }
 
+    public function exists(string $value): bool
+    {
+        foreach ($this->storages as $storage) {
+            $path = Str::replace('@', $value, $storage['format']);
+            $disk = Storage::disk($storage['disk']);
+
+            if ($disk->exists($path)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function allExists(string $value): bool
+    {
+        foreach ($this->storages as $storage) {
+            $path = Str::replace('@', $value, $storage['format']);
+            $disk = Storage::disk($storage['disk']);
+
+            if (!$disk->exists($path)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public function put(string $value, string $content): void
     {
         foreach ($this->storages as $storage) {
@@ -58,5 +87,22 @@ class StorageService
 
             $disk->put($path, $content);
         }
+    }
+
+    /**
+     * @throws StorageContentMissingException
+     */
+    public function download(string $value): StreamedResponse
+    {
+        foreach ($this->storages as $storage) {
+            $disk = Storage::disk($storage['disk']);
+            $path = Str::replace('@', $value, $storage['format']);
+
+            if ($disk->exists($path)) {
+                return $disk->download($path);
+            }
+        }
+
+        throw new StorageContentMissingException();
     }
 }
