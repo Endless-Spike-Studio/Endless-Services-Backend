b7f06655e9f1ddb70b3878944b42673476012412
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-06-08 20:41:12 +0800

    feat(migrations): mark ip as nullable

--- database/migrations/2022_06_07_220325_add_ip_to_gdcs_account_failed_logs_table.php
+++ database/migrations/2022_06_07_220325_add_ip_to_gdcs_account_failed_logs_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('gdcs_account_failed_logs', static function (Blueprint $table) {
-            $table->ipAddress('ip');
+            $table->ipAddress('ip')->nullable();
         });
     }
 
