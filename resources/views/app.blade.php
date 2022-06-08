10d15f486206fe84b89711821dbbfb108d0217be
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-06-08 09:02:48 +0800

    refactor(frontend): using app name to instead hardcode title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,19 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Geometry Dash Chinese</title>
-    @env('local')
-        @vite
-    @endenv
-
-    @env(['stagging', 'production'])
-        <link rel="stylesheet" href="{{ vite_entry('resources/scss/main.scss') }}">
-        <script type="module" src="{{ vite_entry('resources/scripts/main.ts') }}"></script>
-
-        <script nomodule src="{{ vite_entry('resources/scss/main-legacy.scss') }}"></script>
-        <script nomodule src="{{ vite_entry('resources/scripts/main-legacy.ts') }}"></script>
-        <script nomodule src="{{ vite_entry('vite/legacy-polyfills') }}"></script>
-    @endenv
+    <title>{{ config('app.name') }}</title>
+    @vite
+    @inertiaHead
 </head>
 <body class="h-full">
 @inertia
