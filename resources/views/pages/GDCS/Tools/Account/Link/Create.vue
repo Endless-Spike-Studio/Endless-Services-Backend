3568bc46ee28331d8261fd85483c6aa639dba944
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-06-10 08:21:48 +0800

    fix(frontend): change default value

--- resources/views/pages/GDCS/Tools/Account/Link/Create.vue
+++ resources/views/pages/GDCS/Tools/Account/Link/Create.vue
@@ -8,68 +8,68 @@ import {map} from "lodash-es";
 
 const el = ref<FormInst>();
 watchEffect(() => {
-  el.value?.validate();
+    el.value?.validate();
 });
 
 const rules = {
-  server: {
-    type: 'string',
-    required: true,
-    validator: () => Promise.reject(form.errors.server)
-  },
-  name: {
-    type: 'string',
-    required: true,
-    validator: () => Promise.reject(form.errors.name)
-  },
-  password: {
-    type: 'string',
-    required: true,
-    validator: () => Promise.reject(form.errors.password)
-  }
+    server: {
+        type: 'string',
+        required: true,
+        validator: () => Promise.reject(form.errors.server)
+    },
+    name: {
+        type: 'string',
+        required: true,
+        validator: () => Promise.reject(form.errors.name)
+    },
+    password: {
+        type: 'string',
+        required: true,
+        validator: () => Promise.reject(form.errors.password)
+    }
 };
 
 const serverOptions = map(servers, server => {
-  return {
-    label: `${server.name} [${server.id}]`,
-    value: server.host
-  }
+    return {
+        label: `${server.name} [${server.id}]`,
+        value: server.host
+    }
 });
 
 const form = useForm({
-  server: 'www.boomlings.com',
-  name: null,
-  password: null
+    server: 'http://www.boomlings.com/database',
+    name: null,
+    password: null
 });
 </script>
 
 <template layout="GDCS">
-  <n-card class="lg:w-1/3 mx-auto" title="创建新链接">
-    <n-form ref="el" :model="form" :rules="rules">
-      <n-tabs animated type="line">
-        <n-tab-pane name="switch" tab="简单">
-          <n-form-item label="服务器" path="server">
-            <n-select v-model:value="form.server" :options="serverOptions"/>
-          </n-form-item>
-        </n-tab-pane>
-        <n-tab-pane name="custom" tab="高级自定义">
-          <n-form-item label="服务器" path="server">
-            <n-input v-model:value="form.server" placeholder="www.boomlings.com"/>
-          </n-form-item>
-        </n-tab-pane>
-      </n-tabs>
+    <n-card class="lg:w-1/3 mx-auto" title="创建新链接">
+        <n-form ref="el" :model="form" :rules="rules">
+            <n-tabs animated type="line">
+                <n-tab-pane name="switch" tab="简单">
+                    <n-form-item label="服务器" path="server">
+                        <n-select v-model:value="form.server" :options="serverOptions"/>
+                    </n-form-item>
+                </n-tab-pane>
+                <n-tab-pane name="custom" tab="高级自定义">
+                    <n-form-item label="服务器" path="server">
+                        <n-input v-model:value="form.server" placeholder="www.boomlings.com"/>
+                    </n-form-item>
+                </n-tab-pane>
+            </n-tabs>
 
-      <n-form-item label="用户名" path="name">
-        <n-input v-model:value="form.name"/>
-      </n-form-item>
+            <n-form-item label="用户名" path="name">
+                <n-input v-model:value="form.name"/>
+            </n-form-item>
 
-      <n-form-item label="密码" path="password">
-        <n-input v-model:value="form.password" type="password"/>
-      </n-form-item>
+            <n-form-item label="密码" path="password">
+                <n-input v-model:value="form.password" type="password"/>
+            </n-form-item>
 
-      <n-button :disabled="form.processing" @click="form.post( route('gdcs.tools.account.link.create.api') )">
-        提交
-      </n-button>
-    </n-form>
-  </n-card>
+            <n-button :disabled="form.processing" @click="form.post( route('gdcs.tools.account.link.create.api') )">
+                提交
+            </n-button>
+        </n-form>
+    </n-card>
 </template>
