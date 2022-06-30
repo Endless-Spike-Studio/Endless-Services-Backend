15c042e31edbdaae44bad0bccc9c61de48fe8ef3
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-06-30 23:34:47 +0800

    feat(model): apply api token and permissions

--- app/Models/GDCS/Account.php
+++ app/Models/GDCS/Account.php
@@ -16,11 +16,13 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
 
 class Account extends Authenticatable implements MustVerifyEmailContract
 {
-    use HasFactory, MustVerifyEmail, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, MustVerifyEmail, Notifiable, HasPermissions, HasRoles;
 
     protected $table = 'gdcs_accounts';
     protected $fillable = ['name', 'email', 'password'];
