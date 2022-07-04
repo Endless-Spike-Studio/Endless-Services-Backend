56961ba1cc53924faa0b9219e492dff1b8479bcb
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-07-04 15:04:51 +0800

    fix(presenters): no type hints

--- app/Http/Presenters/GDCS/Dashboard/AccountPresenter.php
+++ app/Http/Presenters/GDCS/Dashboard/AccountPresenter.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Presenters\GDCS\Dashboard;
 
+use App\Models\GDCS\Account;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -10,14 +11,13 @@ class AccountPresenter
 {
     public function renderProfile(): Response
     {
+        /** @var Account $account */
+        $account = Auth::guard('gdcs')->user();
+
         return Inertia::render('GDCS/Account/Profile', [
             'gdcs' => [
-                'account' => Auth::guard('gdcs')
-                    ->user()
-                    ->only(['id', 'name', 'email', 'email_verified_at', 'created_at']),
-                'user' => Auth::guard('gdcs')
-                    ->user()
-                    ->load('user:id,name,uuid,udid,created_at')
+                'account' => $account->only(['id', 'name', 'email', 'email_verified_at', 'created_at']),
+                'user' => $account->load('user:id,name,uuid,udid,created_at')
                     ->getRelation('user')
             ]
         ]);
@@ -25,21 +25,23 @@ class AccountPresenter
 
     public function renderSetting(): Response
     {
+        /** @var Account $account */
+        $account = Auth::guard('gdcs')->user();
+
         return Inertia::render('GDCS/Account/Setting', [
             'gdcs' => [
-                'account' => Auth::guard('gdcs')
-                    ->user()
-                    ->only(['id', 'name', 'email'])
+                'account' => $account->only(['id', 'name', 'email'])
             ]
         ]);
     }
 
     public function renderFailedLogs(): Response
     {
+        /** @var Account $account */
+        $account = Auth::guard('gdcs')->user();
+
         return Inertia::render('GDCS/Account/FailedLog', [
-            'logs' => Auth::guard('gdcs')
-                ->user()
-                ->load('failedLogs:id,account_id,content,ip,created_at')
+            'logs' => $account->load('failedLogs:id,account_id,content,ip,created_at')
                 ->getRelation('failedLogs')
         ]);
     }
