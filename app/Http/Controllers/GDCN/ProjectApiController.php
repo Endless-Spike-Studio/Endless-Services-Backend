bc4743da9119c1dafd1ba3a43298d445ea13d2ec
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-07-04 09:56:17 +0800

    feat(project): internal command execute

--- app/Http/Controllers/GDCN/ProjectApiController.php
+++ app/Http/Controllers/GDCN/ProjectApiController.php
@@ -10,6 +10,22 @@ class ProjectApiController extends Controller
     public function update(ProjectUpdateRequest $request): void
     {
         $request->validated();
-        exec('php /app/vendor/bin/envoy run update');
+
+        $commands = [
+            'cd /app',
+            'git pull',
+            'composer install --no-dev',
+            'php artisan optimize:clear',
+            'php artisan migrate',
+            'pnpm install',
+            'pnpm run build',
+            'php artisan static:upload',
+            'php artisan optimize',
+            'php artisan octane:reload'
+        ];
+
+        foreach ($commands as $command) {
+            exec($command);
+        }
     }
 }
