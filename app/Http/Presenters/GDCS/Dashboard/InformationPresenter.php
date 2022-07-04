740da197de90b8504f69477b13be92581d728804
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-07-04 15:44:24 +0800

    fix(backend): Server error

--- app/Http/Presenters/GDCS/Dashboard/InformationPresenter.php
+++ app/Http/Presenters/GDCS/Dashboard/InformationPresenter.php
@@ -15,9 +15,9 @@ class InformationPresenter
     public function renderUser(User $user): Response
     {
         return Inertia::render('GDCS/Dashboard/User/Info', [
-            'user' => $user->select(['id', 'name', 'uuid', 'created_at'])
-                ->load('account:id,name')
+            'user' => $user->load('account:id,name')
                 ->load('score:id,user_id,stars,demons,creator_points,coins,user_coins,updated_at')
+                ->only(['id', 'name', 'uuid', 'created_at', 'account', 'score'])
         ]);
     }
 
@@ -31,15 +31,15 @@ class InformationPresenter
         }
 
         return Inertia::render('GDCS/Dashboard/Level/Info', [
-            'level' => $level->select(['id', 'user_id', 'name', 'desc', 'downloads', 'likes', 'version', 'length', 'password', 'audio_track', 'song_id', 'original_level_id', 'two_player', 'objects', 'coins', 'requested_stars', 'unlisted', 'ldm', 'created_at', 'updated_at'])
-                ->load('user:id,name')
+            'level' => $level->load('user:id,name')
                 ->load('song:id,song_id,name')
                 ->load('original:id,name')
                 ->load('rating:id,level_id,difficulty,featured_score,epic,demon_difficulty,auto,demon,stars,coin_verified,created_at')
                 ->load('comments:id,account_id,level_id,comment,likes,created_at')
                 ->load('comments.account:id,name')
                 ->load('daily:id,level_id,apply_at')
-                ->load('weekly:id,level_id,apply_at'),
+                ->load('weekly:id,level_id,apply_at')
+                ->only(['id', 'user_id', 'name', 'desc', 'downloads', 'likes', 'version', 'length', 'password', 'audio_track', 'song_id', 'original_level_id', 'two_player', 'objects', 'coins', 'requested_stars', 'unlisted', 'ldm', 'created_at', 'updated_at', 'user', 'song', 'original', 'rating', 'comments', 'daily', 'weekly']),
             'permission' => [
                 'rate' => $account->can('RATE_LEVEL'),
                 'mark' => $account->can('MARK_LEVEL')
@@ -50,9 +50,9 @@ class InformationPresenter
     public function renderAccount(Account $account): Response
     {
         return Inertia::render('GDCS/Dashboard/Account/Info', [
-            'account' => $account->select(['id', 'name', 'created_at'])
-                ->load('comments:id,account_id,comment,likes,created_at')
+            'account' => $account->load('comments:id,account_id,comment,likes,created_at')
                 ->load('user:id,name,uuid')
+                ->only(['id', 'name', 'created_at', 'comments', 'user'])
         ]);
     }
 }
