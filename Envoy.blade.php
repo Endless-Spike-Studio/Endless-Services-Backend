e21aeedc5c9659e8e9fac24505fab3eafb6670b0
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-07-06 21:21:26 +0800

    feat(envoy): extract task

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,16 +1,57 @@
 @servers(['web' => 'localhost'])
 
-@task('deploy')
-cd /app
+@task('update-code')
 git fetch --all
 git reset --hard origin/main
 git pull
-composer update --no-dev
-php artisan optimize:clear
-php artisan migrate
+@endtask
+
+@task('update-frontend-packages')
 pnpm update
+@endtask
+
+@task('update-backend-packages')
+composer update --no-dev
+@endtask
+
+@story('update-packages)
+pnpm update
+@endstory
+
+@task('deploy-frontend')
 pnpm run build
 php artisan static:upload
+@endtask
+
+@task('deploy-backend')
+php artisan migrate
+php artisan optimize:clear
 php artisan optimize
+@endtask
+
+@task('restart-server')
 php artisan octane:reload
 @endtask
+
+@story('deploy-frontend')
+update-code
+update-frontend-dependencies
+deploy-frontend
+restart-server
+@endstory
+
+@story('deploy-backend')
+update-code
+update-backend-dependencies
+deploy-backend
+restart-server
+@endstory
+
+@story('deploy')
+update-code
+update-dependencies
+deploy-frontend
+deploy-backend
+restart-server
+@endstory
+
