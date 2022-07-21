6f6e3286e024a9a02d5432a265bc2e1cc0a7575a
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-07-21 17:05:03 +0800

    fix(listener): add new allowed event
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Listeners/ProcessPassword.php
+++ app/Listeners/ProcessPassword.php
@@ -2,13 +2,15 @@
 
 namespace App\Listeners;
 
+use App\Events\GDCS\AccountPasswordChanged;
 use App\Events\GDCS\AccountRegistered;
+use App\Events\UserPasswordChanged;
 use App\Events\UserRegistered;
 use Illuminate\Support\Facades\Hash;
 
 class ProcessPassword
 {
-    public function handle(AccountRegistered|UserRegistered $event): void
+    public function handle(AccountRegistered|UserRegistered|AccountPasswordChanged|UserPasswordChanged $event): void
     {
         $event->model->update([
             'password' => Hash::make($event->model->password),
--- app/Listeners/SendVerificationEmail.php
+++ app/Listeners/SendVerificationEmail.php
@@ -2,14 +2,16 @@
 
 namespace App\Listeners;
 
+use App\Events\GDCS\AccountEmailChanged;
 use App\Events\GDCS\AccountRegistered;
+use App\Events\UserEmailChanged;
 use App\Events\UserRegistered;
 
 class SendVerificationEmail
 {
-    public function handle(AccountRegistered|UserRegistered $event): void
+    public function handle(AccountRegistered|UserRegistered|AccountEmailChanged|UserEmailChanged $event): void
     {
-        if (! $event->model->hasVerifiedEmail()) {
+        if (!$event->model->hasVerifiedEmail()) {
             $event->model->sendEmailVerificationNotification();
         }
     }
