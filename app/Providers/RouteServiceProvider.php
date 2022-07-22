442c7d45e8c57f92816ccad3f3075c6fe3ebe958
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-07-22 21:02:38 +0800

    refactor(app): remove unused rate limiter
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
@@ -15,12 +11,6 @@ class RouteServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        RateLimiter::for('api', static function (Request $request) {
-            $key = Auth::id() ?? $request->ip();
-
-            return Limit::perMinute(60)->by($key);
-        });
-
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
