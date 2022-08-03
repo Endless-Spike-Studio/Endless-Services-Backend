b4d0f5fb9e087f2625ea94f5e79a9c193e10e997
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-03 22:41:07 +0800

    fix(service): response checker fix

--- app/Services/Game/ResponseService.php
+++ app/Services/Game/ResponseService.php
@@ -8,6 +8,6 @@ class ResponseService
 {
     public static function check(string $data): bool
     {
-        return !empty($data) && !Str::startsWith($data, 'error') && !Str::startsWith($data, '<') && !Str::matchAll('/^(-)(\d+)$/', $data)->isEmpty();
+        return empty($data) || Str::startsWith($data, 'error') || Str::startsWith($data, '<') || Str::matchAll('/^(-)(\d+)$/', $data)->isNotEmpty();
     }
 }
