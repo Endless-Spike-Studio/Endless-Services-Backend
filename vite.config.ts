2a65c6f8c0fd1614526fadfddb36bc79277d0f55
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-17 11:41:11 +0800

    style(code): apply eslint fix
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import {defineConfig} from 'vite';
-import tailwindcss from 'tailwindcss';
-import legacy from '@vitejs/plugin-legacy';
-import autoprefixer from 'autoprefixer';
-import laravel from 'vite-plugin-laravel';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import inertia from './resources/scripts/vite/inertia-layout';
-import eslintPlugin from 'vite-plugin-eslint';
+import {defineConfig} from 'vite'
+import tailwindcss from 'tailwindcss'
+import legacy from '@vitejs/plugin-legacy'
+import autoprefixer from 'autoprefixer'
+import laravel from 'vite-plugin-laravel'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import inertia from './resources/scripts/vite/inertia-layout'
+import eslintPlugin from 'vite-plugin-eslint'
 
 export default defineConfig({
     plugins: [
@@ -30,4 +30,4 @@ export default defineConfig({
             exclude: '**'
         })
     ]
-});
+})
