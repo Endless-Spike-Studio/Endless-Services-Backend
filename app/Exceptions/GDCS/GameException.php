e46aa1d6541d2a0aa7d8b04ad665ba5917c160e8
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-19 22:44:38 +0800

    feat(exception): extend game exception class
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

---
+++ app/Exceptions/GDCS/GameException.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Exceptions\GDCS;
+
+use App\Exceptions\BaseException;
+use Throwable;
+
+class GameException extends BaseException
+{
+    public function __construct(
+        string           $message = null,
+        int              $code = 0,
+        Throwable        $previous = null,
+        public           $logging = true,
+        public array     $log_context = [],
+        public int       $http_code = 500,
+        protected string $log_channel = 'gdcn',
+        protected string $log_type = 'notice',
+        protected ?int   $response_code = null
+    )
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function render()
+    {
+        if (!empty($this->response_code)) {
+            return $this->response_code;
+        }
+
+        return parent::render();
+    }
+
+    protected function formatMessage(string $message): string
+    {
+        return 'GDCS 游戏异常: ' . $message;
+    }
+}
