8aaea61576c7304278322e981d3578ede5e9c757
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-21 23:22:50 +0800

    feat(lang): optimize language
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- resources/lang/zh_CN/error.php
+++ resources/lang/zh_CN/error.php
@@ -11,18 +11,18 @@ return [
             'comment' => [
                 'empty' => '账号评论为空',
                 'not_found' => '目标账号评论未找到(或不存在)',
-                'invalid_mode' => '无效模式',
+                'invalid_mode' => '账号评论获取失败, 无效模式',
                 'history' => [
                     'empty' => '账号评论历史为空'
                 ]
             ],
             'not_found' => '账号不存在',
-            'blocked_by_target' => '已被目标拉黑',
-            'mod_permission_not_found' => '没有找到 Mod 权限',
+            'blocked_by_target' => '玩家被目标拉黑',
+            'mod_permission_not_found' => '没有找到对应玩家的 Mod 记录',
             'login' => [
-                'need_verify_email' => '需要验证邮箱'
+                'need_verify_email' => '玩家登录失败, 需要验证邮箱'
             ],
-            'banned' => '已被封禁'
+            'banned' => '操作失败, 玩家已被封禁'
         ]
     ]
 ];
