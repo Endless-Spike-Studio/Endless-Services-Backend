91468e879532ac153fb4e28fcce9da658c2368a9
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-23 22:05:53 +0800

    feat(exception): remove unused register method
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,13 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    public function register(): void
-    {
-        $this->reportable(static function (Throwable $e) {
-        });
-    }
+
 }
