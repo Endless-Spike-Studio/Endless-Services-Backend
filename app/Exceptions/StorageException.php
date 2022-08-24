439d286b3c3c5eeeb78d116404862d07eb3a4800
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-24 20:17:35 +0800

    fix(exception): use class variable to instead unused parameter
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Exceptions/StorageException.php
+++ app/Exceptions/StorageException.php
@@ -6,9 +6,11 @@ use Throwable;
 
 class StorageException extends BaseException
 {
+    protected string $log_channel = 'gdcn';
+
     public function __construct(string $message = null, int $code = 0, Throwable $previous = null, public array $log_context = [])
     {
-        parent::__construct($message, $code, $previous, log_channel: 'gdcn');
+        parent::__construct($message, $code, $previous);
     }
 
     protected function formatMessage(string $message): string
