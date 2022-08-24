cdd3aed2a6f5c44233a8e7dc6b175a41a45a5cd3
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-24 20:50:15 +0800

    feat(marco): using forEach to process data
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use App\Services\Game\BaseGameService;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,14 +14,21 @@ class AppServiceProvider extends ServiceProvider
         Request::macro('context', function () {
             /** @var Request $this */
 
+            $parameters = [];
+            foreach ($this->all() as $key => $value) {
+                if (in_array($key, ['gjp', 'password'], true)) {
+                    $length = strlen($value);
+                    $parameters[$key] = str_repeat('*', $length);
+                    continue;
+                }
+
+                $parameters[$key] = $value;
+            }
+
             return [
                 'ip' => $this->ip(),
                 'url' => $this->fullUrl(),
-                'data' => collect(
-                    $this->except(['gjp', 'password'])
-                )->map(function (string $value) {
-                    return Str::limit($value);
-                })
+                'data' => $parameters
             ];
         });
 
