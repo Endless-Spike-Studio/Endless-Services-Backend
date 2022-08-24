0bbf57b1019bda6c3b80324f2811d520a9a1459f
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-24 20:38:41 +0800

    feat(exception): allow $response to nullable and report response
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Exceptions/GeometryDashChineseServerException.php
+++ app/Exceptions/GeometryDashChineseServerException.php
@@ -9,14 +9,15 @@ class GeometryDashChineseServerException extends BaseException
     protected string $log_channel = 'gdcn';
 
     public function __construct(
-        string     $message = null,
-        int        $code = 0,
-        Throwable  $previous = null,
-        array      $log_context = [],
-        int|string $response = 0
+        string          $message = null,
+        int             $code = 0,
+        Throwable       $previous = null,
+        array           $log_context = [],
+        int|string|null $response = null
     )
     {
         parent::__construct($message, $code, $previous, 500, $log_context);
+        $this->log_context = array_merge(['response' => $response], $this->log_context);
     }
 
     public function render()
