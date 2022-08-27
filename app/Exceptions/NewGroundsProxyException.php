a78b9f891b005bd9e8b3d70c6b8f3bbd17764377
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-27 16:06:39 +0800

    feat(exception): add public readonly response variable
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Exceptions/NewGroundsProxyException.php
+++ app/Exceptions/NewGroundsProxyException.php
@@ -2,9 +2,23 @@
 
 namespace App\Exceptions;
 
+use Throwable;
+
 class NewGroundsProxyException extends BaseException
 {
     protected string $log_channel = 'gdcn';
+
+    public function __construct(
+        string                          $message = null,
+        int                             $code = 0,
+        Throwable                       $previous = null,
+        int                             $http_code = 500,
+        array                           $log_context = [],
+        public readonly int|string|null $response = null
+    )
+    {
+        parent::__construct($message, $code, $previous, $http_code, $log_context);
+    }
 
     protected function formatMessage(string $message): string
     {
