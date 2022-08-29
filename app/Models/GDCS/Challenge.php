6a574b06208079dffb075e0941216c8255c1f5f0
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-29 10:47:10 +0800

    fix(model): no fillable attributes
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Models/GDCS/Challenge.php
+++ app/Models/GDCS/Challenge.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Challenge extends Model
 {
     protected $table = 'gdcs_challenges';
+    protected $fillable = ['type', 'collect', 'name', 'reward'];
 }
--- app/Models/GDCS/LikeRecord.php
+++ app/Models/GDCS/LikeRecord.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class LikeRecord extends Model
 {
     protected $table = 'gdcs_like_records';
+    protected $fillable = ['type', 'item_id', 'user_id', 'ip'];
 }
