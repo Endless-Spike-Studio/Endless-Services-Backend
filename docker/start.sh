973ec165c9488d35c8e5820fa21c32b8b745ec43
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-09-04 01:18:37 +0800

    chore(docker): remove docker
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- docker/start.sh
+++
@@ -1,7 +0,0 @@
-php /app/artisan optimize:clear
-php /app/artisan storage:link
-php /app/artisan key:generate
-php /app/artisan migrate
-php /app/artisan optimize
-supervisord
-php /app/artisan octane:start --port=60101
