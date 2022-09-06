16850966a79052cd2facfe549b048ba83af0ee31
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-09-06 20:01:04 +0800

    feat(project): dispatch process
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,9 +6,11 @@ use Symfony\Component\Process\Process;
 
 class ProjectController extends Controller
 {
-    public function update(): int
+    public function update(): void
     {
-        $process = new Process(['vendor/bin/envoy', 'run', 'deploy-backend'], '/app');
-        return $process->run();
+        $this->dispatchSync(function () {
+            $process = new Process(['vendor/bin/envoy', 'run', 'deploy-backend'], '/app');
+            return $process->run();
+        });
     }
 }
