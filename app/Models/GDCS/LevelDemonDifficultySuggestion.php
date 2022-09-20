d1d94602d2b98e379868d59f2babc128fc136c68
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-09-20 19:59:14 +0800

    fix(model): add fillable
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Models/GDCS/LevelDemonDifficultySuggestion.php
+++ app/Models/GDCS/LevelDemonDifficultySuggestion.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class LevelDemonDifficultySuggestion extends Model
 {
     protected $table = 'gdcs_level_demon_difficulty_suggestions';
+    protected $fillable = ['user_id', 'level_id', 'demon_diff', 'ip'];
 }
