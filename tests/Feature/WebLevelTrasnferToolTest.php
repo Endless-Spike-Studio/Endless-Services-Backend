9aed8673bdb9ddf372147342477e48b1edcf1100
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-10-17 11:07:20 +0800

    test(web): remove unfinished test
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- tests/Feature/WebLevelTrasnferToolTest.php
+++ tests/Feature/WebLevelTrasnferToolTest.php
@@ -3,8 +3,3 @@
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
-
-test('关卡转入', function () {
-    $response = $this->get('/');
-    $response->assertStatus(200);
-});
