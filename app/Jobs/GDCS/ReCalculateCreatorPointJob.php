5b238d49b4a87e803e9f772db3744a4fe2af8988
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-11-13 18:31:00 +0800

    fix(job): reset creator point
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Jobs/GDCS/ReCalculateCreatorPointJob.php
+++ app/Jobs/GDCS/ReCalculateCreatorPointJob.php
@@ -3,6 +3,7 @@
 namespace App\Jobs\GDCS;
 
 use App\Models\GDCS\LevelRating;
+use App\Models\GDCS\UserScore;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -19,14 +20,18 @@ class ReCalculateCreatorPointJob implements ShouldQueue
             ->with('level.creator.score')
             ->get();
 
+        UserScore::query()
+            ->where('creator_points', '>', 0)
+            ->update([
+                'creator_points' => 0
+            ]);
+
         foreach ($ratings as $rating) {
-            $score = $rating->level?->creator?->score;
+            $score = $rating->level->creator->score;
 
             if (empty($score)) {
                 continue;
             }
-
-            $score->creator_points = 0;
 
             if ($rating->stars > 0) {
                 $score->creator_points += config('gdcn.game.creator_points.rated', 1);
