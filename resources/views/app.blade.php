010cb7d52140435866833f9bbe8e1197ed680c11
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-11-24 16:14:00 +0800

    feat(frontend): change order
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
-    @inertiaHead
     @vite('resources/scripts/main.ts', '')
+    @inertiaHead
 </head>
 <body>
 @inertia
