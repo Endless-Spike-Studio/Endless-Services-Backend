cf956039bd54a159a9253755a84a3fdb48b017f5
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-11-24 13:31:46 +0800

    feat(command): mkdir if not exists
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Console/Commands/SyncManifestCommand.php
+++ app/Console/Commands/SyncManifestCommand.php
@@ -14,7 +14,14 @@ class SyncManifestCommand extends Command
     public function handle(): int
     {
         $content = Storage::disk('oss')->get('/static/website/manifest.json');
-        fwrite(fopen(public_path('build/manifest.json'), 'w+'), $content);
+        $fileName = public_path('build/manifest.json');
+
+        $path = dirname($fileName);
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        fwrite(fopen($fileName, 'w+'), $content);
 
         return Command::SUCCESS;
     }
