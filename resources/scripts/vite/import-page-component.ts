dc200a1cc921db88749eddf8925d7818dc2d2aff
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-11-24 15:48:58 +0800

    feat(frontend): remove unused file
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- resources/scripts/vite/import-page-component.ts
+++
@@ -1,9 +0,0 @@
-export function importPageComponent(name: string, pages: Record<string, any>) {
-    for (const path in pages) {
-        if (path.endsWith(`${name.replaceAll('.', '/')}.vue`)) {
-            return typeof pages[path] === 'function' ? pages[path]() : pages[path];
-        }
-    }
-
-    throw new Error(`页面不存在: ${name}`);
-}
