5bd3b5bc417aa04e396042c04be9f11468c404e2
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-12-25 18:14:46 +0800

    feat(frontend): remove scss entry from view
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
-    @vite(['resources/styles/main.scss', 'resources/scripts/main.ts'])
+    @vite(['resources/scripts/main.ts'])
     @inertiaHead
 </head>
 <body>
