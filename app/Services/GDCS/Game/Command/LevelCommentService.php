57cfd5673d609f789645d9d36caef8d8e4206cb1
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-12-26 17:15:18 +0800

    fix(command): wrong column
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Services/GDCS/Game/Command/LevelCommentService.php
+++ app/Services/GDCS/Game/Command/LevelCommentService.php
@@ -179,7 +179,7 @@ class LevelCommentService extends BaseCommandService
         }
 
         $this->level->audio_track = $newValue;
-        $this->level->song = 0;
+        $this->level->song_id = 0;
         $this->level->save();
 
         return __('gdcn.game.command.level_update_success');
@@ -201,7 +201,7 @@ class LevelCommentService extends BaseCommandService
         }
 
         $this->level->audio_track = 0;
-        $this->level->song = $newValue;
+        $this->level->song_id = $newValue;
         $this->level->save();
 
         return __('gdcn.game.command.level_update_success');
