fecb3bc3eaeadfec8a0309af475eb5361b9e84b8
渣渣120 <WOSHIZHAZHA120@qq.com>
2023-01-02 23:09:01 +0800

    fix(GDCS): forget add fillable attributes
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Models/GDCS/LevelStarSuggestion.php
+++ app/Models/GDCS/LevelStarSuggestion.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class LevelStarSuggestion extends Model
 {
     protected $table = 'gdcs_level_star_suggestions';
+    protected $fillable = ['user_id', 'level_id', 'stars', 'ip'];
 }
