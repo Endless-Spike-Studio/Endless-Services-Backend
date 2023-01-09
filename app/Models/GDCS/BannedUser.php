81f5d2299dcabfc60d3996e0e2c47b83e8d6178d
渣渣120 <WOSHIZHAZHA120@qq.com>
2023-01-09 16:50:34 +0800

    fix(antiCheatService): no fillable attribute caused ban failed
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Models/GDCS/BannedUser.php
+++ app/Models/GDCS/BannedUser.php
@@ -9,13 +9,15 @@ class BannedUser extends Model
 {
     protected $table = 'gdcs_banned_users';
 
+    protected $fillable = ['user_id', 'login_ban', 'reason', 'comment_ban', 'comment_ban_info', 'expires_at'];
+
     protected $dates = [
         'expires_at',
     ];
 
     public function getCommentBanInfoAttribute(): int|string|null
     {
-        if (! empty($this->comment_ban)) {
+        if (!empty($this->comment_ban)) {
             if (empty($this->expires_at)) {
                 return Response::COMMENT_CREATE_FAILED_BANNED->value;
             }
