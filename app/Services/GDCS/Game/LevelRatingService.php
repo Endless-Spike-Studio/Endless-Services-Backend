131fef4615d4615782f76a2583cd8bd194c37e88
渣渣120 <WOSHIZHAZHA120@qq.com>
2023-01-19 19:54:22 +0800

    fix(service): optimize code
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Services/GDCS/Game/LevelRatingService.php
+++ app/Services/GDCS/Game/LevelRatingService.php
@@ -9,25 +9,28 @@ class LevelRatingService
 {
     public static function reCalculateCreatorPoints(): void
     {
+        $relation = 'level.creator.score';
+
         UserScore::query()
             ->update([
                 'creator_points' => 0
             ]);
 
         LevelRating::query()
-            ->with('level.creator')
-            ->whereHas('level.creator.score')
+            ->with($relation)
+            ->whereHas($relation)
             ->each(function (LevelRating $rating) {
-                $score = UserScore::query()
-                    ->where('user_id', $rating->level->creator->id)
-                    ->first();
+                $score = $rating->level
+                    ?->creator
+                    ?->score();
 
                 if (empty($score)) {
                     return;
                 }
 
                 if ($rating->stars > 0) {
-                    $score->creator_points += config('gdcn.game.creator_points.rated');
+                    $amount = config('gdcn.game.creator_points.rated');
+                    $score->increment('creator_points', $amount);
                 }
 
                 if ($rating->featured_score > 0) {
@@ -35,17 +38,16 @@ class LevelRatingService
                     $featuredReward = config('gdcn.game.creator_points.featured.reward');
 
                     if ($multiplyWithScore) {
-                        $score->creator_points += $featuredReward * $rating->featured_score;
+                        $score->increment('creator_points', $featuredReward * $rating->featured_score);
                     } else {
-                        $score->creator_points += $featuredReward;
+                        $score->increment('creator_points', $featuredReward);
                     }
                 }
 
                 if ($rating->epic) {
-                    $score->creator_points += config('gdcn.game.creator_points.epic');
+                    $amount = config('gdcn.game.creator_points.epic');
+                    $score->increment('creator_points', $amount);
                 }
-
-                $score->save();
             });
     }
 }
