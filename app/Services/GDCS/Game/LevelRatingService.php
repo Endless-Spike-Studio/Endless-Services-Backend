8af78f9ee5410855dfc20f69cd76026105e27c31
渣渣120 <WOSHIZHAZHA120@qq.com>
2023-01-19 19:47:12 +0800

    fix(service): optimize code
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Services/GDCS/Game/LevelRatingService.php
+++ app/Services/GDCS/Game/LevelRatingService.php
@@ -9,41 +9,42 @@ class LevelRatingService
 {
     public static function reCalculateCreatorPoints(): void
     {
-        $relation = 'level.creator.score';
-
-        $ratings = LevelRating::query()
-            ->with($relation)
-            ->whereHas($relation)
-            ->get();
-
         UserScore::query()
             ->update([
                 'creator_points' => 0
             ]);
 
-        foreach ($ratings as $rating) {
-            $score = $rating->level->creator->score;
+        LevelRating::query()
+            ->with('level.creator.score')
+            ->each(function (LevelRating $rating) {
+                $score = $rating->level
+                    ?->creator
+                    ?->score;
 
-            if ($rating->stars > 0) {
-                $score->creator_points += config('gdcn.game.creator_points.rated');
-            }
-
-            if ($rating->featured_score > 0) {
-                $multiplyWithScore = config('gdcn.game.creator_points.featured.multiply_with_score');
-                $featuredReward = config('gdcn.game.creator_points.featured.reward');
-
-                if ($multiplyWithScore) {
-                    $score->creator_points += $featuredReward * $rating->featured_score;
-                } else {
-                    $score->creator_points += $featuredReward;
+                if (empty($score)) {
+                    return;
                 }
-            }
 
-            if ($rating->epic) {
-                $score->creator_points += config('gdcn.game.creator_points.epic');
-            }
+                if ($rating->stars > 0) {
+                    $score->creator_points += config('gdcn.game.creator_points.rated');
+                }
 
-            $score->save();
-        }
+                if ($rating->featured_score > 0) {
+                    $multiplyWithScore = config('gdcn.game.creator_points.featured.multiply_with_score');
+                    $featuredReward = config('gdcn.game.creator_points.featured.reward');
+
+                    if ($multiplyWithScore) {
+                        $score->creator_points += $featuredReward * $rating->featured_score;
+                    } else {
+                        $score->creator_points += $featuredReward;
+                    }
+                }
+
+                if ($rating->epic) {
+                    $score->creator_points += config('gdcn.game.creator_points.epic');
+                }
+
+                $score->save();
+            });
     }
 }
