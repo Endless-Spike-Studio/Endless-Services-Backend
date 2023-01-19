ccb478492acbe1ff7f08e82a91e325ccbdce456a
渣渣120 <WOSHIZHAZHA120@qq.com>
2023-01-19 19:41:17 +0800

    fix(service): optimize code
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Services/GDCS/Game/LevelRatingService.php
+++ app/Services/GDCS/Game/LevelRatingService.php
@@ -19,15 +19,10 @@ class LevelRatingService
             ]);
 
         foreach ($ratings as $rating) {
-            $score = $rating->level?->creator?->score;
+            $amount = 0;
 
-            if (empty($rating->level) || empty($score)) {
-                continue;
-            }
-
-            $cp = 0;
             if ($rating->stars > 0) {
-                $cp += config('gdcn.game.creator_points.rated');
+                $amount += config('gdcn.game.creator_points.rated');
             }
 
             if ($rating->featured_score > 0) {
@@ -35,17 +30,20 @@ class LevelRatingService
                 $featuredReward = config('gdcn.game.creator_points.featured.reward');
 
                 if ($multiplyWithScore) {
-                    $cp += $featuredReward * $rating->featured_score;
+                    $amount += $featuredReward * $rating->featured_score;
                 } else {
-                    $cp += $featuredReward;
+                    $amount += $featuredReward;
                 }
             }
 
             if ($rating->epic) {
-                $cp += config('gdcn.game.creator_points.epic');
+                $amount += config('gdcn.game.creator_points.epic');
             }
 
-            $score->increment('creator_points', $cp);
+            $rating->level
+                ?->creator
+                ?->score()
+                ->increment('creator_points', $amount);
         }
     }
 }
