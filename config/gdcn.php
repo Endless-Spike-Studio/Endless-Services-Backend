c72869a191db578f8f84c172f67a004736d39f41
渣渣120 <WOSHIZHAZHA120@qq.com>
2023-02-05 20:20:14 +0800

    fix(GDCS): set 100001 as default weekly id offset
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- config/gdcn.php
+++ config/gdcn.php
@@ -35,7 +35,7 @@ return [
     ],
     'game' => [
         'per_page' => 10,
-        'weekly_offset' => 100000,
+        'weekly_offset' => 100001,
         'custom_song_offset' => 10000000,
         'creator_points' => [
             'rated' => 1,
