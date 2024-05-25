59bc887460308697458c6d6b6a0656ad567e2d1d
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-05-26 00:18:54 +0800

    :recycle: 重写 `RouteServiceProvider`

--- Modules/GeometryDashProxy/app/Providers/RouteServiceProvider.php
+++ Modules/GeometryDashProxy/app/Providers/RouteServiceProvider.php
@@ -2,48 +2,18 @@
 
 namespace Modules\GeometryDashProxy\Providers;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * Called before routes are registered.
-     *
-     * Register any model bindings or pattern based filters.
-     */
-    public function boot(): void
-    {
-        parent::boot();
-    }
-
-    /**
-     * Define the routes for the application.
-     */
-    public function map(): void
-    {
-        $this->mapApiRoutes();
-
-        $this->mapWebRoutes();
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     */
-    protected function mapWebRoutes(): void
-    {
-        Route::middleware('web')->group(module_path('GeometryDashProxy', '/routes/web.php'));
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     */
-    protected function mapApiRoutes(): void
-    {
-        Route::middleware('api')->prefix('api')->name('api.')->group(module_path('GeometryDashProxy', '/routes/api.php'));
-    }
+	public function map(): void
+	{
+		Route::middleware('api')
+			->prefix('api')
+			->name('api.')
+			->group([
+				module_path('GeometryDashProxy', '/routes/api.php')
+			]);
+	}
 }
