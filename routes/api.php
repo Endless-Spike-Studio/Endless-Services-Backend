04c403a8dd2fa771e4553183f7dda50622a02c88
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-05-27 15:33:00 +0800

    :sparkles: 添加 `GeometryDashProxy` 路由

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,9 @@
 <?php
+
+use App\GeometryDashProxy\Controllers\GameApiController;
+
+Route::group([
+	'prefix' => 'GeometryDashProxy'
+], function () {
+	Route::post('/{path}', [GameApiController::class, 'proxy'])->where('path', '.*');
+});
