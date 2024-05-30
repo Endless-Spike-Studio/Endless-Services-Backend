4a0d985e7cd60cc3ee44daa7649f0f492c039631
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-05-30 08:30:26 +0800

    :sparkles: 使用 `Laravel 11` 的 `bootstrap`

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,13 @@
 <?php
 
-$app = new Illuminate\Foundation\Application(
-	$_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
+use Illuminate\Foundation\Application;
 
-$app->singleton(Illuminate\Contracts\Http\Kernel::class, App\Http\Kernel::class);
-$app->singleton(Illuminate\Contracts\Console\Kernel::class, App\Console\Kernel::class);
-$app->singleton(Illuminate\Contracts\Debug\ExceptionHandler::class, App\Exceptions\Handler::class);
-
-return $app;
+return Application::configure(
+	basePath: dirname(__DIR__)
+)
+	->withRouting(
+		api: __DIR__ . '/../routes/api.php'
+	)
+	->withMiddleware()
+	->withExceptions()
+	->create();
