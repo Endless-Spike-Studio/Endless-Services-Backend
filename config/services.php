2357b6ea7a6280e59c501fb58d7dad204173b91b
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-06-01 22:03:17 +0800

    :sparkles: 添加配置文件

---
+++ config/services.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'endless' => [
+		'proxy' => [
+			'server' => env('ENDLESS_PROXY_SERVER'),
+			'retry' => [
+				'times' => env('ENDLESS_PROXY_RETRY_TIMES', 10),
+				'delay' => env('ENDLESS_PROXY_RETRY_DELAY', 0)
+			],
+			'geometry_dash' => [
+				'base' => env('ENDLESS_PROXY_GEOMETRY_DASH_BASE', 'https://www.boomlings.com/database')
+			]
+		]
+	]
+];
