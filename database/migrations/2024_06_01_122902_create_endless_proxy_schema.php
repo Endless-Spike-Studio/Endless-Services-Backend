5c2d50f6940b15e1f892fe736c947151e073e913
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-07-03 23:04:09 +0800

    :sparkles: 提取 `name`

--- database/migrations/2024_06_01_122902_create_endless_proxy_schema.php
+++ database/migrations/2024_06_01_122902_create_endless_proxy_schema.php
@@ -4,13 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
+	protected readonly string $name = 'endless_proxy';
+
 	public function up(): void
 	{
-		DB::unprepared('CREATE SCHEMA endless_proxy');
+		DB::unprepared("CREATE SCHEMA $this->name");
 	}
 
 	public function down(): void
 	{
-		DB::unprepared('DROP SCHEMA IF EXISTS endless_proxy');
+		DB::unprepared("DROP SCHEMA IF EXISTS $this->name");
 	}
 };
