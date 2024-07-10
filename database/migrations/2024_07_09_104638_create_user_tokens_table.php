80ec78accbff6ad450fbd063c0a574527e19cc1d
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-07-10 21:34:29 +0800

    :sparkles: 添加 `users` 和 `user_tokens` 表

---
+++ database/migrations/2024_07_09_104638_create_user_tokens_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Base\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	protected string $name = 'user_tokens';
+
+	public function up(): void
+	{
+		Schema::create($this->name, function (Blueprint $table) {
+			$table->id();
+			$table->foreignIdFor(User::class)->unique();
+			$table->string('name');
+			$table->string('token', 64)->unique();
+			$table->timestamp('expires_at')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	public function down(): void
+	{
+		Schema::dropIfExists($this->name);
+	}
+};
