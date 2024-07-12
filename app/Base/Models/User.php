17d048bf5f7760766b297fa8bdb52847e3ac9b4e
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-07-12 22:46:33 +0800

    :sparkles: 自动哈希密码

--- app/Base/Models/User.php
+++ app/Base/Models/User.php
@@ -3,9 +3,15 @@
 namespace App\Base\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Model
 {
 	protected $table = 'public.users';
 	protected $fillable = ['name', 'email', 'password'];
+
+	public function setPasswordAttribute(string $password): void
+	{
+		$this->attributes['password'] = Hash::make($password);
+	}
 }
