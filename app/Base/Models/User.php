8df910a9b4f6a01bc525f761fc1dc0aa8df762b3
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-08-06 21:47:32 +0800

    :sparkles: 使用新的 `hashed` 转换器

--- app/Base/Models/User.php
+++ app/Base/Models/User.php
@@ -3,15 +3,16 @@
 namespace App\Base\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Hash;
 
 class User extends Model
 {
 	protected $table = 'public.users';
 	protected $fillable = ['name', 'email', 'password'];
 
-	public function setPasswordAttribute(string $password): void
+	protected function casts(): array
 	{
-		$this->attributes['password'] = Hash::make($password);
+		return [
+			'password' => 'hashed'
+		];
 	}
 }
