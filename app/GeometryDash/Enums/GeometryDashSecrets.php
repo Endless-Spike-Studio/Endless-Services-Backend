6366d5550aa2f2f91bc8ac472689a3fb766a352a
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-08-06 21:47:48 +0800

    :sparkles: 新增 `GeometryDashSecrets` 枚举

---
+++ app/GeometryDash/Enums/GeometryDashSecrets.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\GeometryDash\Enums;
+
+enum GeometryDashSecrets: string
+{
+	case COMMON = 'Wmfd2893gb7';
+	case ACCOUNT = 'Wmfv3899gc9';
+	case LEVEL = 'Wmfv2898gc9';
+	case MOD = 'Wmfp3879gc3';
+	case ADMIN = 'Wmfx2878gb9';
+}
