546fe653e1434697f42a41bc2204f4415245074d
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-08-30 22:01:07 +0800

    :art: 优化代码

--- database/migrations/2024_06_01_122902_create_endless_proxy_schema.php
+++ database/migrations/2024_06_01_122902_create_endless_proxy_schema.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
-	protected  string $name = 'endless_proxy';
+	protected string $name = 'endless_proxy';
 
 	public function up(): void
 	{
