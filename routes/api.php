4da555b9263ef9eecea059e1dc3a4429392a8142
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-08-30 22:02:54 +0800

    :sparkles: 新增接口占位

--- routes/api.php
+++ routes/api.php
@@ -62,3 +62,67 @@ Route::group([
 		});
 	});
 });
+
+Route::group([
+	'prefix' => 'EndlessServer'
+], function () {
+	Route::group([
+		'prefix' => 'GeometryDash'
+	], function () {
+		// Route::post('/accounts/registerGJAccount.php', []);
+		// Route::post('/accounts/loginGJAccount.php', []);
+		// Route::post('/updateGJUserScore22.php', []);
+		// Route::post('/getGJUserInfo20.php', []);
+		// Route::post('/updateGJAccSettings20.php', []);
+		// Route::post('/uploadGJAccComment20.php', []);
+		// Route::post('/getGJAccountComments20.php', []);
+		// Route::post('/deleteGJAccComment20.php', []);
+		// Route::post('/getAccountURL.php', []);
+		// Route::post('/database/accounts/backupGJAccountNew.php', []);
+		// Route::post('/database/accounts/syncGJAccountNew.php', []);
+		// Route::post('/getGJRewards.php', []);
+		// Route::post('/requestUserAccess.php', []);
+		// Route::post('/getGJChallenges.php', []);
+		// Route::post('/getGJUsers20.php', []);
+		// Route::post('/getGJScores20.php', []);
+		// Route::post('/uploadGJMessage20.php', []);
+		// Route::post('/getGJMessages20.php', []);
+		// Route::post('/downloadGJMessage20.php', []);
+		// Route::post('/deleteGJMessages20.php', []);
+		// Route::post('/uploadGJLevel21.php', []);
+		// Route::post('/getGJLevels21.php', []);
+		// Route::post('/uploadFriendRequest20.php', []);
+		// Route::post('/readGJFriendRequest20.php', []);
+		// Route::post('/downloadGJLevel22.php', []);
+		// Route::post('/reportGJLevel.php', []);
+		// Route::post('/deleteGJLevelUser20.php', []);
+		// Route::post('/updateGJDesc20.php', []);
+		// Route::post('/uploadGJComment21.php', []);
+		// Route::post('/getGJComments21.php', []);
+		// Route::post('/deleteGJComment20.php', []);
+		// Route::post('/deleteGJFriendRequests20.php', []);
+		// Route::post('/getGJFriendRequests20.php', []);
+		// Route::post('/blockGJUser20.php', []);
+		// Route::post('/unblockGJUser20.php', []);
+		// Route::post('/getGJUserList20.php', []);
+		// Route::post('/removeGJFriend20.php', []);
+		// Route::post('/acceptGJFriendRequest20.php', []);
+		// Route::post('/getGJCommentHistory.php', []);
+		// Route::post('/getGJMapPacks21.php', []);
+		// Route::post('/getGJDailyLevel.php', []);
+		// Route::post('/getGJGauntlets21.php', []);
+		// Route::post('/likeGJItem211.php', []);
+		// Route::post('/getGJLevelScores211.php', []);
+		// Route::post('/rateGJStars211.php', []);
+		// Route::post('/rateGJDemon21.php', []);
+		// Route::post('/suggestGJStars20.php', []);
+		// Route::post('/getGJSongInfo.php', []);
+		// Route::post('/restoreGJItems.php', []);
+		// Route::post('/getGJTopArtists.php', []);
+		// Route::post('/deleteGJLevelList.php', []);
+		// Route::post('/getCustomContentURL.php', []);
+		// Route::post('/getGJLevelLists.php', []);
+		// Route::post('/getGJLevelScoresPlat.php', []);
+		// Route::post('/uploadGJLevelList.php', []);
+	});
+});
