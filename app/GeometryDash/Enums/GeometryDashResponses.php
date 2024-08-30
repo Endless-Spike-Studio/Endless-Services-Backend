13ce9efb045a7dcbcbf161b6e8961581ce0864d7
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-08-30 23:52:53 +0800

    :art: 优化代码

--- app/GeometryDash/Enums/GeometryDashResponses.php
+++ app/GeometryDash/Enums/GeometryDashResponses.php
@@ -4,15 +4,17 @@ namespace App\GeometryDash\Enums;
 
 enum GeometryDashResponses: int
 {
-	case SUCCESS = 1;
-	case FAILED = -1;
+	case __1 = 1;
 
+	case _1 = -1;
 	case _2 = -2;
+	case _11 = -11;
 	case _12 = -12;
 
-	public const GeometryDashResponses SONG_DISABLED = GeometryDashResponses::_2;
-
-	public const GeometryDashResponses ACCOUNT_REGISTER_SUCCESS = GeometryDashResponses::SUCCESS;
-	public const GeometryDashResponses ACCOUNT_LOGIN_FAILED_ACCOUNT_NOT_FOUND = GeometryDashResponses::FAILED;
+	public const GeometryDashResponses SUCCESS = GeometryDashResponses::__1;
+	public const GeometryDashResponses FAILED = GeometryDashResponses::_2;
+	public const GeometryDashResponses ACCOUNT_REGISTER_SUCCESS = GeometryDashResponses::__1;
+	public const GeometryDashResponses ACCOUNT_LOGIN_FAILED_ACCOUNT_NOT_FOUND = GeometryDashResponses::_11;
 	public const GeometryDashResponses ACCOUNT_LOGIN_FAILED_EMAIL_NOT_VERIFIED = GeometryDashResponses::_12;
+	public const GeometryDashResponses SONG_DISABLED = GeometryDashResponses::_2;
 }
