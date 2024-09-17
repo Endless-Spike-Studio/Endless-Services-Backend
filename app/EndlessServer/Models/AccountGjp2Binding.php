fa8907714c1dac8f6b057b80183c4777e2e9e07a
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-09-17 17:06:43 +0800

    :sparkles: 为 `gjp` 关联表新增账号关联

--- app/EndlessServer/Models/AccountGjp2Binding.php
+++ app/EndlessServer/Models/AccountGjp2Binding.php
@@ -3,8 +3,14 @@
 namespace App\EndlessServer\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AccountGjp2Binding extends Model
 {
 	protected $table = 'endless_server.account_gjp2_bindings';
+
+	public function account(): BelongsTo
+	{
+		return $this->belongsTo(Account::class);
+	}
 }
