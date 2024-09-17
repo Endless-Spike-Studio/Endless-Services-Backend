2d89aad8df733a7edacefe43a7bf170db5bf4f2c
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-09-17 17:09:06 +0800

    :sparkles: 新增一些关卡相关的枚举

---
+++ app/GeometryDash/Enums/GeometryDashLevelLengths.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\GeometryDash\Enums;
+
+enum GeometryDashLevelLengths: int
+{
+	case TINY = 0;
+	case SHORT = 1;
+	case MEDIUM = 2;
+	case LONG = 3;
+	case XL = 4;
+	case PLATFORMER = 5;
+}
---
+++ app/GeometryDash/Enums/GeometryDashLevelRatingDifficulties.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\GeometryDash\Enums;
+
+enum GeometryDashLevelRatingDifficulties: int
+{
+	case NA = 0;
+	case EASY = 10;
+	case NORMAL = 20;
+	case HARD = 30;
+	case HARDER = 40;
+	case INSANE = 50;
+	case AUTO_AND_DEMON = 60;
+
+	public const GeometryDashLevelRatingDifficulties AUTO = GeometryDashLevelRatingDifficulties::AUTO_AND_DEMON;
+	public const GeometryDashLevelRatingDifficulties DEMON = GeometryDashLevelRatingDifficulties::AUTO_AND_DEMON;
+}
