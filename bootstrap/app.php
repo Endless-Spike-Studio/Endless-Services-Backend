5cdacc4ff26ba62e44755011824c9c420be1307e
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-09-28 12:43:56 +0800

    :art: :wrench: 简化代码, 配置广播

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,14 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
 	->withRouting(
-		api: __DIR__ . '/../routes/api.php',
-		health: '/up'
+		api: __DIR__ . '/../routes/api.php'
 	)
-	->withMiddleware(function (Middleware $middleware) {
-
-	})
-	->withExceptions(function (Exceptions $exceptions) {
-
-	})->create();
+	->withBroadcasting('', [
+		'prefix' => 'api'
+	])
+	->withMiddleware()
+	->withExceptions()
+	->create();
