32295fd75086a89a223d2c543c7d60b9417f41ef
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-10-26 15:23:14 +0800

    :sparkles: 创建 `endless_server.account_comments` 表

---
+++ app/EndlessServer/Models/AccountComment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\EndlessServer\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccountComment extends Model
+{
+	protected $table = 'endless_server.account_comments';
+}
