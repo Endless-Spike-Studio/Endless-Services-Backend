8e0426ac5e076cb4638ab8120e03e992087dd850
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-11-05 11:51:05 +0800

    :bug: 修复未设置 `fillable` 导致的报错

--- app/EndlessServer/Models/AccountSetting.php
+++ app/EndlessServer/Models/AccountSetting.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class AccountSetting extends Model
 {
 	protected $table = 'endless_server.account_settings';
+
+	protected $fillable = ['message_state', 'friend_request_state', 'comment_history_state', 'youtube', 'twitch', 'twitter'];
 }
