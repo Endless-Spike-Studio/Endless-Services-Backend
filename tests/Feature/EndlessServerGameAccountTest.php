173d99113c73b6e5fd0c059b05f7f51332f9de2b
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-11-05 11:51:53 +0800

    :white_check_mark: 完善账号功能和账号设置功能的测试

--- tests/Feature/EndlessServerGameAccountTest.php
+++ tests/Feature/EndlessServerGameAccountTest.php
@@ -2,18 +2,23 @@
 
 use App\EndlessServer\Controllers\GameAccountController;
 use App\EndlessServer\Models\Account;
+use App\EndlessServer\Models\AccountGjp2Binding;
 use App\GeometryDash\Enums\GeometryDashResponses;
 use App\GeometryDash\Enums\GeometryDashSecrets;
+use App\GeometryDash\Services\GeometryDashAlgorithmService;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
+use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\post;
 
 test('register', function () {
 	$faker = fake();
 	$faker = $faker->unique();
 
+	$name = $faker->userName;
+
 	$request = post(URL::action([GameAccountController::class, 'register']), [
-		'userName' => $faker->userName,
+		'userName' => $name,
 		'password' => $faker->password,
 		'email' => $faker->safeEmail,
 		'secret' => GeometryDashSecrets::ACCOUNT->value
@@ -23,6 +28,10 @@ test('register', function () {
 	$request->assertContent(
 		(string)GeometryDashResponses::ACCOUNT_REGISTER_SUCCESS->value
 	);
+
+	assertDatabaseHas(Account::class, [
+		'name' => $name,
+	]);
 });
 
 test('login', function () {
@@ -48,8 +57,12 @@ test('login', function () {
 	]);
 
 	$request->assertOk();
-
 	$request->assertContent(
 		implode(',', [$account->id, $account->player->id])
 	);
+
+	assertDatabaseHas(AccountGjp2Binding::class, [
+		'account_id' => $account->id,
+		'gjp2' => app(GeometryDashAlgorithmService::class)->generateGjp2($password)
+	]);
 });
