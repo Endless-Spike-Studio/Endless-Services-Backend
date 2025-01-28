ce97944d53e56467f3ae66cbc9bbf4a5bc06f1ff
渣渣120 <WOSHIZHAZHA120@qq.com>
2025-01-29 01:26:09 +0800

    :sparkles: 新增请求参数

--- app/EndlessServer/Requests/GameAccountCommentDeleteRequest.php
+++ app/EndlessServer/Requests/GameAccountCommentDeleteRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\EndlessServer\Requests;
 
+use App\EndlessServer\Models\Account;
 use App\EndlessServer\Models\AccountComment;
 use App\EndlessServer\Traits\GameRequestRules;
+use App\GeometryDash\Enums\GeometryDashCommentType;
 use Illuminate\Validation\Rule;
 
 class GameAccountCommentDeleteRequest extends GameRequest
@@ -16,10 +18,31 @@ class GameAccountCommentDeleteRequest extends GameRequest
 			...$this->versions(),
 			...$this->gdw(),
 			...$this->auth_gjp2(),
+			'uuid' => [
+				'nullable',
+				'string'
+			],
+			'udid' => [
+				'nullable',
+				'string'
+			],
 			'commentID' => [
 				'required',
 				'integer',
 				Rule::exists(AccountComment::class, 'id')
+			],
+			'cType' => [
+				'required',
+				'integer',
+				Rule::enum(GeometryDashCommentType::class),
+				Rule::in([
+					GeometryDashCommentType::ACCOUNT->value
+				])
+			],
+			'targetAccountID' => [
+				'required',
+				'integer',
+				Rule::exists(Account::class, 'id')
 			],
 			...$this->secret()
 		];
