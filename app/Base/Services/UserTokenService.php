81b5202ff2efbfc96f0f81277d580b3682da18a3
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2025-06-28 16:26:58 +0800

    :sparkles: 确保 `token` 不重复

--- app/Base/Services/UserTokenService.php
+++ app/Base/Services/UserTokenService.php
@@ -11,12 +11,24 @@ class UserTokenService
 {
 	public function create(User $user, string $name, ?DateTime $expires_at = null)
 	{
-		return UserToken::query()
-			->create([
-				'user_id' => $user->id,
-				'name' => $name,
-				'token' => Str::random(64),
-				'expires_at' => $expires_at
-			]);
+		while (true) {
+			$token = Str::random(64);
+
+			$exists = UserToken::query()
+				->where('token', $token)
+				->exists();
+
+			if ($exists) {
+				continue;
+			}
+
+			return UserToken::query()
+				->create([
+					'user_id' => $user->id,
+					'name' => $name,
+					'token' => $token,
+					'expires_at' => $expires_at
+				]);
+		}
 	}
 }
